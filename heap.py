fb7a7e6b86691e7acf8119dd0b992894c30fa2c5
Sergio Oliveira <sergio@tracy.com.br>
2015-04-20 09:07:09 -0300

    Heap implementation

---
+++ heap.py
@@ -0,0 +1,125 @@
+
+class MinHeap(object):
+
+    def __init__(self):
+        self._heap = []
+        self._heap_index = {}
+
+    def __len__(self):
+        return len(self._heap)
+
+    def __iter__(self):
+        return self
+
+    def iter(self):
+        return self
+
+    @property
+    def last_index(self):
+        return len(self) - 1
+
+    @property
+    def root(self):
+        return self._heap[0]
+
+    def swap(self, i, j):
+        # Swapping in Pythonic way
+        self._heap[i], self._heap[j] = self._heap[j], self._heap[i]
+
+    def next(self):
+        if not self._heap:
+            raise StopIteration
+            
+        self.swap(0, self.last_index)
+        popped = self._heap.pop(self.last_index)
+
+        if self._heap:
+            self.bubble_down()
+
+        return popped
+
+    def add(self, value, priority):
+        self._heap.append((priority, value))
+        self.bubble_up()
+
+    def bubble_up(self, current_index=None):
+        if current_index == None:
+            current_index = self.last_index
+
+        current = self._heap[current_index]
+        parent_index, parent = self._get_parent(current_index)
+
+        while parent and parent[0] > current[0]:
+            self.swap(parent_index, current_index)
+
+            current_index = parent_index
+            parent_index, parent = self._get_parent(current_index)
+
+    def bubble_down(self, current_index=None):
+        if current_index == None:
+            current_index = 0
+
+        current = self._heap[current_index]
+        child_index, child = self._get_lowest_priority_child(current_index)
+
+        while child and child[0] < current[0]:
+            self.swap(child_index, current_index)
+
+            current_index = child_index
+            child_index, child = self._get_lowest_priority_child(current_index)
+
+    def _get_parent_index(self, index):
+        if index == 0:
+            return None
+
+        if index % 2 == 0:
+            parent_index = (index // 2) - 1
+        else:
+            parent_index = index // 2
+
+        return parent_index 
+
+    def _get_parent(self, index):
+        parent_index = self._get_parent_index(index)
+
+        if parent_index == None:
+            return None, None
+
+        return parent_index, self._heap[parent_index]
+
+    def _get_children_index(self, index):
+        left = index * 2 + 1
+        if left > self.last_index:
+            left = None
+
+        right = index * 2 + 2
+        if right > self.last_index:
+            right = None
+
+        return left, right
+
+    def _get_children(self, index):
+        left, right = self._get_children_index(index)
+        if left:
+            left_child = self._heap[left]
+        else:
+            left_child = None
+
+        if right:
+            right_child = self._heap[right]
+        else:
+            right_child = None
+
+        return (left, left_child), (right, right_child)
+
+    def _get_lowest_priority_child(self, index): 
+        children = self._get_children(index)
+        (left_index, left_child), (right_index, right_child) = children 
+
+        if not any((left_child, right_child)):
+            return None, None
+
+        if not right_child or left_child[0] < right_child[0]:
+            return left_index, left_child
+
+        return right_index, right_child
---
+++ tests/test_heap.py
@@ -0,0 +1,91 @@
+
+import unittest
+
+from heap import MinHeap
+
+
+class TestHeap(unittest.TestCase):
+
+    def setUp(self):
+        self.heap = MinHeap()
+
+    def test_get_parent_index(self):
+        self.assertEqual(self.heap._get_parent_index(0), None)
+        self.assertEqual(self.heap._get_parent_index(1), 0)
+        self.assertEqual(self.heap._get_parent_index(2), 0)
+        self.assertEqual(self.heap._get_parent_index(3), 1)
+        self.assertEqual(self.heap._get_parent_index(4), 1)
+        self.assertEqual(self.heap._get_parent_index(5), 2)
+        self.assertEqual(self.heap._get_parent_index(6), 2)
+        self.assertEqual(self.heap._get_parent_index(7), 3)
+        self.assertEqual(self.heap._get_parent_index(8), 3)
+        self.assertEqual(self.heap._get_parent_index(9), 4)
+        self.assertEqual(self.heap._get_parent_index(100), 49)
+        self.assertEqual(self.heap._get_parent_index(101), 50)
+
+    def test_add(self):
+        self.heap.add(value=10, priority=10)
+        self.assertEqual(self.heap.root, (10, 10))
+        self.heap.add(value=20, priority=20)
+        self.assertEqual(self.heap.root, (10, 10))
+        self.heap.add(value=5, priority=5)
+        self.assertEqual(self.heap.root, (5, 5))
+        self.heap.add(value=7, priority=7)
+        self.assertEqual(self.heap.root, (5, 5))
+        self.heap.add(value=3, priority=3)
+        self.assertEqual(self.heap.root, (3, 3))
+
+        heap = [(3, 3), (5, 5), (10, 10), (20, 20), (7, 7)]
+        self.assertEqual(self.heap._heap, heap)
+
+    def test_len(self):
+        self.assertEqual(len(self.heap), 0)
+        self.heap.add(value=10, priority=10)
+        self.assertEqual(len(self.heap), 1)
+        self.heap.add(value=15, priority=15)
+        self.heap.add(value=20, priority=20)
+        self.heap.add(value=25, priority=25)
+        self.assertEqual(len(self.heap), 4)
+
+    def test_get_children_index(self):
+        self.assertEqual(self.heap._get_children_index(0), (None, None))
+        self.heap.add(value=10, priority=10)
+        self.assertEqual(self.heap._get_children_index(0), (None, None))
+        self.heap.add(value=15, priority=15)
+        self.assertEqual(self.heap._get_children_index(0), (1, None))
+        self.heap.add(value=20, priority=20)
+        self.assertEqual(self.heap._get_children_index(0), (1, 2))
+        for i in range(21):
+            self.heap.add(value=i, priority=i)
+
+        self.assertEqual(self.heap._get_children_index(5), (11, 12))
+        self.assertEqual(self.heap._get_children_index(11), (23, None))
+
+    def test_next(self):
+        self.heap.add(value=10, priority=10)
+        self.heap.add(value=20, priority=20)
+        self.heap.add(value=5, priority=5)
+        self.heap.add(value=7, priority=7)
+        self.heap.add(value=3, priority=3)
+
+        self.assertEqual(self.heap.next(), (3, 3))
+        self.assertEqual(self.heap.root, (5, 5))
+        self.assertEqual(self.heap.next(), (5, 5))
+        self.assertEqual(self.heap.root, (7, 7))
+        self.assertEqual(self.heap.next(), (7, 7))
+        self.assertEqual(self.heap.root, (10, 10))
+        self.heap.next()
+        self.assertEqual(self.heap.root, (20, 20))
+        self.heap.next()
+
+        with self.assertRaises(StopIteration):
+            self.heap.next()
+
+    def test_next_with_right_lowest(self):
+        self.heap.add(value=10, priority=10)
+        self.heap.add(value=5, priority=5)
+        self.heap.add(value=3, priority=3)
+        self.heap.add(value=15, priority=5)
+
+        self.assertEqual(self.heap.next(), (3, 3))
+        self.assertEqual(self.heap.root, (5, 5))
